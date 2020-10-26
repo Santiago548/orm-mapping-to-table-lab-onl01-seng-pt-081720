c0d6d5e629c2d7768cef79b0548af40148e22ae1
Dominic Barajas <dominicbarajas548@gmail.com>
2020-10-26 17:46:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,8 @@ class Student
 
   def save
     sql = <<-SQL
-        INSERT INTO students (name, grade) VALUES ()?, ?)
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
 
         SQL
   end
